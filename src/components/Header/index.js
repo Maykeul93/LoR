bcf9bd2bef2e274a81fa0e22524474388c54bbf4
Maykeul93 <michael.mittoo@gmail.com>
2021-03-18 14:42:06 +0100

    Ajout NavBar

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+
+import PrimarySearchAppBar from './NavBar';
+
+const Header = () => (
+  <Box component="div">
+    <PrimarySearchAppBar />
+  </Box>
+);
+
+export default Header;
---
+++ src/components/Lor/index.js
@@ -0,0 +1,16 @@
+// == Import npm
+import React from 'react';
+
+import Header from 'src/components/Header';
+// == Import
+import './styles.scss';
+
+// == Composant
+const Lor = () => (
+  <div className="lor">
+    <Header />
+  </div>
+);
+
+// == Export
+export default Lor;
